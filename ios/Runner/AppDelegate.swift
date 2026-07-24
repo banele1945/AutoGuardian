09c6e7ab5ee930d41a56b184b74a895213a04bb2
Banele Mdladla <231108338@mycput.ac.za>
2026-07-24 10:26:15 +0200

    Remove API key from AppDelegate.swift
    Removed Google Maps API key from AppDelegate.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCHY7LNy9HoLWNyVj4Ab6heE2rywssd0hk")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
